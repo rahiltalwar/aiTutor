ae1d0dcce8bb097b467d3d63404f953e9f71e1d3
rahil <rahil.talwar@gmail.com>
2026-02-05 06:13:15 -0500

    first commit

---
+++ src/main/java/com/ai/tutor/aiTutor/AiTutorApplication.java
@@ -0,0 +1,13 @@
+package com.ai.tutor.aiTutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTutorApplication.class, args);
+	}
+
+}
